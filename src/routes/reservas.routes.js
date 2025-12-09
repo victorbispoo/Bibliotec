efd238a5362df58df5f5385dff2287da815be64a
GuedesSiilva <davig0400@gmail.com>
2025-12-09 11:04:10 -0300

    reservas funcionais

--- src/routes/reservas.routes.js
+++ src/routes/reservas.routes.js
@@ -2,13 +2,15 @@ import express from "express";
 import {
     listarReservas,
     criarReserva,
-    DeletarReservas
+    DeletarReservas,
+    ListarReservasDeLivrosID
 } from "../controllers/reservas.controllers.js";
 
 const router = express.Router();
 
 
 router.get("/", listarReservas);
+router.get("/:id", ListarReservasDeLivrosID);
 router.post("/", criarReserva);
 router.delete("/:id", DeletarReservas);
 
