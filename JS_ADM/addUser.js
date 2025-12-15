d23ce8ee278f2a800c539cf03a2c97104879305d
victorbispoo <dlsejogosdaoras@gmail.com>
2025-12-15 20:34:41 -0300

    Correção dos erros do server

---
+++ JS_ADM/addUser.js
@@ -0,0 +1,27 @@
+document.getElementById("formCadastroUser").addEventListener("submit", async e => {
+    e.preventDefault();
+  
+    const dataNascimento = document.getElementById("data_nascimento").value || null;
+  
+    const resposta = await fetch("http://localhost:3000/usuarios", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        nome: document.getElementById("nome").value,
+        email: document.getElementById("email").value,
+        senha: document.getElementById("senha").value,
+        data_nascimento: dataNascimento,
+        perfil: document.getElementById("perfil").value
+      })
+    });
+  
+    console.log("Data enviada:", dataNascimento);
+    console.log("REQ.BODY:", req.body);
+    if (resposta.ok) {
+      alert("Usuário cadastrado");
+      document.getElementById("formCadastroUser").reset();
+    } else {
+      const erro = await resposta.json();
+      alert("Erro ao cadastrar: " + erro.erro);
+    }
+  });
